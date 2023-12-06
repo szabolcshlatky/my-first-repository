743496a8a72711ec759df096cc84b4564e1eaba5
szabolcshlatky <szabolcshlatky@gmail.com>
2023-12-06 14:32:29 +0100

    branch 30 added

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,12 @@
 declare const $: any;
 import './main.scss';
 
-let object = {
-  colors: [`orange`, `magenta`, `red`, `brown`, `green`, `blue`, `teal`],
-  hasManyColors: true,
-};
+let szam = 0;
 
-/* Események és eseményfigyelők */
-
-$(`.click`).click(() => {
-  console.log(`Igen, most rámkattintottál`);
+$(`.fel`).click(() => {
+  $(`h2`).text(szam++);
 });
 
-$(`.secondBtn`).on("click", () => {
-  $(`.click`).text(`Igen, most rámkattintottál`);
-});
-
-$(`.thirdBtn`).on("click", () => {
-  $(`.click`).css(`background-color`, object.colors[1]);
-  $(`.secondBtn`).css(`background-color`, object.colors[3]);
-  $(`.thirdBtn`).css(`background-color`, object.colors[6]);
-
-  /* Adj új funkciókat az eseményfigyelőidhez */
-
-  let input = document.getElementById("color").value;
-  $(`#color`).css(`background-color`, input);
-
-  $(`.thirdBtn`).attr(`disabled="disabled"`);
+$(`.le`).click(() => {
+  $(`h2`).text(szam--);
 });
